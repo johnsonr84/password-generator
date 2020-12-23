09bef82eb9aaa74e3f9861012a0cc0fb64396e85
Robert Johnson <robertcjohnson1984@gmail.com>
2020-12-22 20:35:55 -0700

    add function to generate password to text area

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 generateBtn = document.querySelector("generate");
 
+// function writes password to the text area
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+}
+
 // add event listener to generate button
 generateBtn.addEventListener("click",)
